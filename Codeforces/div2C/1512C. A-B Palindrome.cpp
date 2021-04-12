5db2f8f74cc49ffdb59720c6a67232409d704c25
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-04-12 06:41:27 -0700

    Add the working soln for 1512C. Implementation problem.

--- Codeforces/div2C/1512C. A-B Palindrome.cpp
+++ Codeforces/div2C/1512C. A-B Palindrome.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<algorithm>
 
 using namespace std;
 
@@ -8,68 +9,69 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int a, b, one = 0, zero = 0, ques = 0, i , j;
+        int a, b, one = 0, zero = 0, ques = 0, i , j, flag = 0;
         string s;
-        cin>>a>>b>>s;   
-        for(i = 0; i < s.size(); i++){
-            if(s[i] == '0') zero++;
-            else if(s[i] == '1') one++;
-            else if(s[i] == '?') ques++;
-        }     
-        if(ques == 0){
+        cin>>a>>b>>s; 
+        for (size_t times = 0; times < 2; times++)
+        { 
+            for(i = 0; i < s.size(); i++){
+                j = s.size() - i -1;
+                if(s[i] != '?'){
+                    if(s[j] == '?') s[j] = s[i];
+                    else if(s[i] != s[j]){
+                        cout<<-1<<endl;
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag == 1) break;
+            }
+            if(flag == 1) break;
+            reverse(s.begin(), s.end());
+        }
+        if (flag == 1) continue;
+        a -= count(s.begin(), s.end(), '0');
+        b -= count(s.begin(), s.end(), '1');
+        ques = count(s.begin(), s.end(), '?');
+        bool isMidEmpty = (s.size() % 2 == 1 && s[s.size() / 2] == '?');
+        if(a < 0 || b < 0 || a + b != ques || isMidEmpty && a % 2 != 0 && b % 2 != 0){
             cout<<-1<<endl;
             continue;
         }
-        int sum = (a-zero) + (b-one);
-        if((ques - sum) != 0) cout<<-1<<endl;
-        else{
-            zero = a-zero;
-            one = b-one;
-            for(i = 0, j = s.size()-1; i < j; i++, j--){
-                if(s[i] == '0' && s[j] == '?'){
-                    s[j] = '0';
-                    zero--;
-                }
-                else if(s[i] == '?' && s[j] == '0'){
-                    s[i] = '0';
-                    zero--;
-                }
-                else if(s[i] == '?' && s[j] == '1'){
-                    s[i] = '1';
-                    one--;
-                }
-                else if(s[i] == '1' && s[j] == '?'){
-                    s[j] = '1';
-                    one--;
-                }
-                else if(s[i] == '?' && s[j] == '?'){
-                    if(zero >= 2) {
-                        s[i] = '0';
-                        s[j] = '0';
-                        zero -= 2;
-                    }
-                    else{
-                        s[i] = '1';
-                        s[j] = '1';
-                        one -= 2;
-                    }
-                }
-            }
-            if(one == 1 && s.size() % 2 != 0){
-                s[i] = '1';
-                cout<<s<<endl;
-            }
-            else if(zero == 1 && s.size() % 2 != 0){
-                s[i] = '0';
-                cout<<s<<endl;
-            }
-            else if((zero >= 1 || one >= 1) && s.size() % 2 == 0){
+        if(a % 2 == 1 || b % 2 == 1){
+            if(s[s.size() / 2] != '?'){
                 cout<<-1<<endl;
+                flag = 1;   
             }
-            else {
-                cout<<s<<endl;
+            else if(a % 2 == 1) {
+                s[s.size() / 2] = '0';
+                a--;
+            }
+            else{
+                s[s.size() / 2] = '1';
+                b--;
             }
         }
+        if(flag == 1) continue;
+        if(a % 2 == 1 || b % 2 == 1){
+            cout<<-1<<endl;
+            continue;
+        }
+        for(i = 0; i < s.size(); i++){
+            if(s[i] == '?'){
+                if(a > 0){
+                    s[i] = s[s.size() - i - 1] = '0';
+                    a-=2;
+                }
+                else
+                {
+                    s[i] = s[s.size() - i - 1] = '1';
+                    b-=2;
+                }
+                
+            }
+        }
+        cout<<s<<endl;
     }
     return 0;
 }
