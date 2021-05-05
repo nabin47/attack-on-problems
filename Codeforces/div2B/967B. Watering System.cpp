eace823c8221e32c8e4291eb94c58407da4544c9
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-05-05 03:23:09 -0700

    Add the soln of 967B. Math and sorting problem.

--- Codeforces/div2B/967B. Watering System.cpp
+++ Codeforces/div2B/967B. Watering System.cpp
@@ -6,25 +6,32 @@
 using namespace std;
 
 int main() {
-    int n, a, b, i, c = 0;
-    long long sum = 0, back_sum = 0;
+    int n, a, b, i, c = 0, signal = 0;
+    long long sum = 0;
     cin>>n>>a>>b;
     vector<int> x(n);
-    int s = 0;
+
     for(i = 0; i < n; i++) {
         cin>>x[i];
-        if(i == 0) {
-            s = ceil((x[i] * a) / (double)b);
-        }
+        
         sum += x[i];
     }
+    int div = x[0] * a, pre = x[0];
     sort(x.begin(), x.end());
-    for(i = n - 1; i >= 0; i--) {
-        if(sum - back_sum < s) {
-            back_sum += x[i];
-            c++;
-        }
+    if((div / sum) >= b) {
+        cout<<0<<endl;
     }
-    cout<<c<<endl;
+    else{
+        for(i = n - 1; i >= 0; i--) {
+            if(x[i] == pre and signal == 0) {
+                signal = 1;
+                continue;
+            }
+            sum -= x[i];
+            c++;
+            if((div / sum) >= b) break;
+        }
+        cout<<c<<endl;
+    }
     return 0;
 }
